9b749d5e1e992a13d182de69f7758efadddef980
Ian Fuller-Thomson <ianfullerthomson@gmail.com>
2024-05-11 23:26:25 -0400

    Files i forgot to add earlier

---
+++ Globals/Game.gd
@@ -0,0 +1,9 @@
+extends Node
+
+#These are the variables that deterimnes the default time that character
+#has until the lights die around the player
+const DEFAULTLIGHTTIME := 15.0
+#lightTime is not a true repersentation of how much time has elapsed, it will
+#stop decrementing once the final size of the light is reached
+var lightTime := DEFAULTLIGHTTIME
+
