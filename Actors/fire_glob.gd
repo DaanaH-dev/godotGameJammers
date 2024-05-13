f9861766c0a572a538befef7e6cfff747fa21da7
dhonarma <68249577+DaanaH-dev@users.noreply.github.com>
2024-05-12 21:09:44 -0400

    Figured out projectiles somewhat, implemented two ideas and going to figure out how rigid body's work

--- Actors/fire_glob.gd
+++ Actors/fire_glob.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
 
-var speed = 750
+var speed = 150
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
+#Currently updates the position to move along the x axis relative to the glob itself
 func _physics_process(delta):
 	position += transform.x * speed * delta
+	position+= global_transform.y 
