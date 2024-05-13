753afa2203a72b282f22f0514f8891256a7d60d3
Ian Fuller-Thomson <ianfullerthomson@gmail.com>
2024-05-13 17:04:49 -0400

    Added paticles to the the projectile

--- Actors/fire_glob_v_2.gd
+++ Actors/fire_glob_v_2.gd
@@ -2,10 +2,15 @@ extends RigidBody2D
 
 var speed = 700
 var gravityScale = 1
+var stopMoving = false
 @onready var player = get_node("../Player/That Guy")
+@onready var groundCollision = $GroundDetection
+@onready var contactEffect = $Explosion
+@onready var animation = $AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	animation.play("Flying")
+	set_lock_rotation_enabled(true)
 	set_gravity_scale(gravityScale)
 	var force = Vector2.ZERO 
 	var direction = (get_global_mouse_position() - player.global_position).normalized()
@@ -15,9 +20,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#position += transform.x
-	pass
+	#position += transform.x	
+	if stopMoving == true:
+		set_freeze_enabled(true)
+		
+		
 	
 	
 	
 	
+
+
+
+
+func _on_ground_detection_body_entered(body):
+	stopMoving = true
+	contactEffect.set_emitting(true)
+	if(animation != null):
+		animation.queue_free()
+	if($CollisionShape2D != null):
+		$CollisionShape2D.queue_free()
+	print("Frozen")
